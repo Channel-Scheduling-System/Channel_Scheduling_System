14680c06ff2d306024bfd6ddffe09c25bbe55210
Edison <edison.gutierrez01@uptc.edu.co>
2026-03-22 11:18:16 -0500

    AB#56 feat(auth): add register method to AuthService

--- frontend/src/app/features/auth/interfaces/auth-service.interface.ts
+++ frontend/src/app/features/auth/interfaces/auth-service.interface.ts
@@ -1,8 +1,13 @@
 import { Observable } from 'rxjs';
 import { LoginRequest } from '../models/requests/login/login-request.model';
 import { LoginResponse } from '../models/responses/login/login-response.model';
+import { RegisterRequest } from '../models/requests/register/register-request.model';
+import { RegisterResponse } from '../models/responses/register/register-response.model';
 
 export interface IAuthService {
   
   login(credentials: LoginRequest): Observable<LoginResponse>;
+
+  register(credentials: RegisterRequest): Observable<RegisterResponse>;
+
 }
