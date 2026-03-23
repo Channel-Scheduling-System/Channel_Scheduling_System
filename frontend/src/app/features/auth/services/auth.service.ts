f4b65c621a33b26b304dec0be01caf08bc4d1131
Edison <edison.gutierrez01@uptc.edu.co>
2026-03-22 22:40:25 -0500

    AB#95 refactor(auth): remove register method, now in UserService

--- frontend/src/app/features/auth/services/auth.service.ts
+++ frontend/src/app/features/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 import { API_ENDPOINTS } from '../../../shared/constants/api-endpoints.constants';
 import { LoginRequest, LoginRequestSchema } from '../models/requests/login/login-request.model';
@@ -8,21 +8,16 @@ import { LoginResponse, LoginResponseSchema } from '../models/responses/login/lo
 import { ResponseHandler } from '../../../core/utils/handlers/response.handler';
 import { HttpErrorHandler } from '../../../core/utils/handlers/error.handler';
 import { TokenService } from '../../../core/services/token.service';
-import { SessionService } from '../../../core/services/session.service';
-import { IAuthService} from '../interfaces/auth-service.interface';
-import { RegisterRequest, RegisterRequestBaseSchema } from '../models/requests/register/register-request.model';
-import { RegisterResponse, RegisterResponseSchema } from '../models/responses/register/register-response.model';
+import { IAuthService } from '../interfaces/auth-service.interface';
 
 @Injectable({ providedIn: 'root' })
-export class AuthService implements IAuthService{
-
+export class AuthService implements IAuthService {
   constructor(
     private http: HttpClient,
     private responseHandler: ResponseHandler,
     private errorHandler: HttpErrorHandler,
-    private tokenService: TokenService,
-    private sessionService: SessionService
-  ) { }
+    private tokenService: TokenService
+  ) {}
 
   login(credentials: LoginRequest): Observable<LoginResponse> {
     const validatedRequest = LoginRequestSchema.parse(credentials);
@@ -34,17 +29,4 @@ export class AuthService implements IAuthService{
       catchError(error => this.errorHandler.handleError(error))
     );
   }
-
-  register(credentials: RegisterRequest): Observable<RegisterResponse> {
-    const validatedRequest = RegisterRequestBaseSchema.parse(credentials);
-    return this.http.post(API_ENDPOINTS.AUTH.REGISTER, validatedRequest).pipe(
-      map(response => this.responseHandler.handleSuccess(response, RegisterResponseSchema)),
-      tap((response: RegisterResponse) => {
-        this.tokenService.setToken(response.data.token);
-        this.sessionService.setSession(response.data.user);
-      }),
-      catchError(error => this.errorHandler.handleError(error))
-    );
-  }
-
 }
