83efc64935d5cbc35e08377cafec1e2f422cc9dd
Edison <edison.gutierrez01@uptc.edu.co>
2026-03-22 22:40:11 -0500

    AB#95 feat(app): check admin existence before session restore

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SessionService } from './core/services/session.service';
+import { AdminService } from './core/services/admin.service';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,35 @@ import { SessionService } from './core/services/session.service';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit { 
+export class App implements OnInit {
   protected readonly title = signal('frontend');
 
   constructor(
-    private sessionService: SessionService
-  ) {}
+    private sessionService: SessionService,
+    private adminService: AdminService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.sessionService.initAuth().subscribe({
-      error: () => {}
+    this.adminService.checkAdminExists().subscribe({
+      next: (response) => {
+        if (!response.data.exists) {
+          this.router.navigate(['/auth/admin-register']);
+        }else {
+          this.sessionService.initAuth().subscribe({
+            error: () => { 
+              this.router.navigate(['/auth/login']);
+            }
+          });
+        }
+      },
+      error: () => {
+        this.sessionService.initAuth().subscribe({
+          error: () => {
+            this.router.navigate(['/auth/login']);
+           }
+        });
+      }
     });
   }
 }
