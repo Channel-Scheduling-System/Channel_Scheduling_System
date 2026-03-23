0bd1b4d1b441bbbfd993dabe9375843167b1e734
Edison <edison.gutierrez01@uptc.edu.co>
2026-03-22 22:39:41 -0500

    AB#95 feat(core): add AdminService to check admin existence

---
+++ frontend/src/app/core/services/admin.service.ts
@@ -0,0 +1,25 @@
+// core/services/admin.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { API_ENDPOINTS } from '../../shared/constants/api-endpoints.constants';
+import { ResponseHandler } from '../utils/handlers/response.handler';
+import { HttpErrorHandler } from '../utils/handlers/error.handler';
+import { AdminExistsResponse, AdminExistsResponseSchema } from '../../shared/models/admin/admin-exists-response.model';
+
+@Injectable({ providedIn: 'root' })
+export class AdminService {
+  constructor(
+    private http: HttpClient,
+    private responseHandler: ResponseHandler,
+    private errorHandler: HttpErrorHandler
+  ) {}
+
+  checkAdminExists(): Observable<AdminExistsResponse> {
+    return this.http.get(API_ENDPOINTS.ADMIN.EXISTS).pipe(
+      map(response => this.responseHandler.handleSuccess(response, AdminExistsResponseSchema)),
+      catchError(error => this.errorHandler.handleError(error))
+    );
+  }
+}
