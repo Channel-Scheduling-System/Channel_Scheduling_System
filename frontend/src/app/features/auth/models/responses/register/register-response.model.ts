3ff6d8f4692f0b4ca14ad576a56008b8353af27c
Edison <edison.gutierrez01@uptc.edu.co>
2026-03-22 22:41:01 -0500

    AB#95 refactor(auth): remove deprecated auth models, now in users module

--- frontend/src/app/features/auth/models/responses/register/register-response.model.ts
+++
@@ -1,6 +0,0 @@
-import { AuthData, AuthResponseSchema } from "../auth-response.model";
-import { AuthResponse } from "../auth-response.model";
-
-export type RegisterData = AuthData;
-export const RegisterResponseSchema = AuthResponseSchema;
-export type RegisterResponse = AuthResponse;
