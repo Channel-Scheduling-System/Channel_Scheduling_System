05ad8cbdae2d014f7ed2f4085bdccabf9f9ffd9a
Edison <edison.gutierrez01@uptc.edu.co>
2026-03-24 11:20:27 -0500

    WIP: added services of service creation

--- frontend/src/app/features/services/services/services.service.ts
+++ frontend/src/app/features/services/services/services.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map, catchError} from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { API_ENDPOINTS } from '../../../shared/constants/api-endpoints.constants';
 import { ResponseHandler } from '../../../core/utils/handlers/response.handler';
 import { HttpErrorHandler } from '../../../core/utils/handlers/error.handler';
 import { ServicesListResponse, ServicesListResponseSchema } from '../models/responses/services-list-response.model';
+import { CreateServiceRequest, CreateServiceRequestSchema } from '../models/requests/create-service-request';
+import { CreateServiceResponse, CreateServiceResponseSchema } from '../models/responses/create-service-response';
 
 @Injectable({ providedIn: 'root' })
 export class ServicesService {
-  constructor(
-    private http: HttpClient, 
-    private responseHandler: ResponseHandler, 
-    private errorHandler: HttpErrorHandler
-  ) { }
+    constructor(
+        private http: HttpClient,
+        private responseHandler: ResponseHandler,
+        private errorHandler: HttpErrorHandler
+    ) { }
     getServicesByWorker(workerId: number): Observable<ServicesListResponse> {
         return this.http.get(API_ENDPOINTS.SERVICES.BY_WORKER(workerId)).pipe(
             map(response => this.responseHandler.handleSuccess(response, ServicesListResponseSchema)),
             catchError(error => this.errorHandler.handleError(error))
         );
     }
+
+    createService(request: CreateServiceRequest): Observable<CreateServiceResponse> {
+        const validatedRequest = CreateServiceRequestSchema.parse(request);
+        return this.http.post(API_ENDPOINTS.SERVICES.CREATE, validatedRequest).pipe(
+            map(response => this.responseHandler.handleSuccess(response, CreateServiceResponseSchema)),
+            catchError(error => this.errorHandler.handleError(error))
+        );
+    }
+
 }
