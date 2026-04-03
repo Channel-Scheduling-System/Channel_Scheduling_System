bbbcf87d1120d3aba9e8350a69a3b5b361117112
Edison <edison.gutierrez01@uptc.edu.co>
2026-04-02 19:39:06 -0500

    AB#126 refactor(routes): add no-auth guard and role-based route protection

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
-import { ServicesPageComponent } from './features/services/pages/services/services.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { RoleGuard } from './core/guards/role.guards';
+import { ROLES } from './core/constants/roles.constants';
+import { noAuthGuard } from './core/guards/no-auth.guard';
+
+const [ADMIN, WORKER, CLIENT] = ROLES;
 
 export const routes: Routes = [
     { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
     {
         path: 'auth',
+        canActivate: [noAuthGuard],
         loadChildren: () => import('./features/auth/auth-module').then(m => m.AuthModule)
+    },
+    {
+        path: 'home',
+        component: MainLayoutComponent,
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./features/home/home-module').then(m => m.HomeModule)
     },
     {
         path: '',
@@ -19,7 +29,7 @@ export const routes: Routes = [
                 path: 'services', 
                 loadChildren: () => import('./features/services/services-module').then(m => m.ServicesModule),
                 canActivate: [RoleGuard],
-                data: { roles: ['WORKER'] }
+                data: { roles: [WORKER] }
             },
             { path: '', redirectTo: 'services', pathMatch: 'full' }
         ]
