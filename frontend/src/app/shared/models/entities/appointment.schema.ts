d235d470be50f507348d281a68ee606c7c51d5f7
Edison <edison.gutierrez01@uptc.edu.co>
2026-05-26 22:01:12 -0500

    AB#182 feat(shared): add base entity, date and appointment schemas; add appointments API endpoints

---
+++ frontend/src/app/shared/models/entities/appointment.schema.ts
@@ -0,0 +1,35 @@
+import z from 'zod';
+import { EntityId } from './entity-base.schema';
+import { serviceDuration, servicePrice } from './service.schema';
+
+export const AppointmentDateTime = z.iso.datetime({ precision: -1 });
+
+export const AppointmentServiceSchema = z.object({
+	serviceId: EntityId,
+	customDuration: serviceDuration,
+	customPrice: servicePrice
+});
+
+export const AppointmentSchema = z.object({
+	workerId: EntityId,
+	clientId: EntityId,
+	startAt: AppointmentDateTime,
+	services: z.array(AppointmentServiceSchema),
+	notes: z.string()
+        .max(300, 'Las notas no pueden exceder 300 caracteres')
+        .optional()
+});
+
+export const AppointmentStatus = z.enum([
+	'PENDING',
+	'REJECTED',
+	'SCHEDULED',
+	'IN_PROGRESS',
+	'CANCELLED',
+	'COMPLETED',
+	'NO_SHOW',
+]);
+export type AppointmentStatus = z.infer<typeof AppointmentStatus>;
+
+export type AppointmentService = z.infer<typeof AppointmentServiceSchema>;
+export type Appointment = z.infer<typeof AppointmentSchema>;
