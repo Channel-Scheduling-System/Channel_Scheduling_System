112b635c83303b1d60707d167afd2a87472e254f
JohanGil1502 <johan.gil@uptc.edu.co>
2026-06-02 00:03:40 -0500

    Change backend production url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://agenda-channel.online/api',
+  apiUrl: 'https://agenda-channel.online/api',
   apiTimeout: 30000,
   powerBi: {
     revenuePerService: 'https://app.powerbi.com/view?r=eyJrIjoiZTRkOWNjZDctMTFlYS00YzQ3LWJhMmEtY2ViM2EzOTU2N2ZjIiwidCI6ImM2ZTIwZTQzLWNmY2QtNDg5ZS1hMjY2LTQxOGY1NmRmMTI0YyIsImMiOjR9&pageName=d088b1e922fb51b0ba4e',
